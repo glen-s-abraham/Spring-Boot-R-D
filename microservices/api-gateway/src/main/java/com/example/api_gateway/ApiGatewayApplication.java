9c44bb71084d067998762cbb624f931c9774e855
iss211 <glen.abraham@immcossi.in>
2026-03-17 15:18:32 +0530

    feat(microservice):omplete microservice code

---
+++ microservices/api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,41 @@
+package com.example.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-feign/(?<segment>.*)",
+                                "/currency-conversion/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+
+}
---
+++ microservices/currency-exchange-microservice/src/main/java/com/example/currency_exchange_microservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.example.currency_exchange_microservice.repository;
+
+import com.example.currency_exchange_microservice.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
